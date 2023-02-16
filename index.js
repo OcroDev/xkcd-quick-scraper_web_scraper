c936573f2e95c8938020766386b4adea63efd69a
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2023-02-16 16:15:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import fs from "fs-extra";
+import axios from "axios";
+import { getImageSize } from "./getImageSize.js";
+import { log, time } from "./log.js";
+
+const endTime = time("ended ");
+
+const { writeJSON } = fs;
+
+const INITIAL_ID_XKCD_COMIC = 2650;
+const MAX_ID_XKCD_COMIC = 2738;
+
+for (
+  let comicId = INITIAL_ID_XKCD_COMIC;
+  comicId <= MAX_ID_XKCD_COMIC;
+  comicId++
+) {
+  const url = `https://xkcd.com/${comicId}/info.0.json`;
+  log(`Fetching ${url}...`);
+  const { data } = await axios.get(url);
+  const { num: id, news, transcript, img, ...restOfComic } = data;
+  log(`Fetched comic #${id}. Getting image dimensions...`);
+  const { height, width } = await getImageSize({ url: img });
+  log(`Got image dimensions: ${height}x${width}`);
+  const comicToStore = {
+    id,
+    img,
+    height,
+    width,
+    ...restOfComic,
+  };
+  const jsonFile = `./comics/${id}.json`;
+  await writeJSON(jsonFile, comicToStore);
+  log(`Wrote ${jsonFile}! ✅ \n`);
+}
+
+endTime();
